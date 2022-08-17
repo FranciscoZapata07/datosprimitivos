f657517a503c9a42fc065bba423b3c37261093a2
Maria Paulina Otalvaro <motalvarot@unal.edu.co>
2022-08-17 17:34:24 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,14 @@
+#hola me llamo comentario e linea
+
+'''
+hola soy un comentario de bloque
+'''
+#datos primitivos
+nombre="Noah"
+edad=33
+estatura=1.63
+hinchadelmejor=True
+
+#salidas
+print("hola: ",nombre)
+print(f'hola {nombre} tu estatura es {estatura}')
